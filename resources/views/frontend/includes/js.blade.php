e6c5c3c0882f53e1ca307c481581794e1fd6fba3
rimbaborne <rimbaborne@gmail.com>
2021-11-23 08:09:16 +0800

    tambah pengaturan map leaflet & js

--- resources/views/frontend/includes/js.blade.php
+++ resources/views/frontend/includes/js.blade.php
@@ -12,3 +12,181 @@
     });
     });
 </script>
+
+{{-- Tambah data dana --}}
+<script>
+function addRowDana() {
+    /* Declare variables */
+    var elements, templateRow, rowCount, row, className, newRow, element;
+    var i, s, t;
+
+    /* Get and count all "tr" elements with class="row".    The last one will
+     * be serve as a template. */
+    if (!document.getElementsByTagName)
+        return false; /* DOM not supported */
+    elements = document.getElementsByTagName("tr");
+    templateRow = null;
+    rowCount = 0;
+    for (i = 0; i < elements.length; i++) {
+        row = elements.item(i);
+
+        /* Get the "class" attribute of the row. */
+        className = null;
+        if (row.getAttribute)
+            className = row.getAttribute('class')
+        if (className == null && row.attributes) {    // MSIE 5
+            /* getAttribute('class') always returns null on MSIE 5, and
+             * row.attributes doesn't work on Firefox 1.0.    Go figure. */
+            className = row.attributes['class'];
+            if (className && typeof(className) == 'object' && className.value) {
+                // MSIE 6
+                className = className.value;
+            }
+        }
+
+        /* This is not one of the rows we're looking for.    Move along. */
+        if (className != "row_to_clone_dana")
+            continue;
+
+        /* This *is* a row we're looking for. */
+        templateRow = row;
+        rowCount++;
+    }
+    if (templateRow == null)
+        return false; /* Couldn't find a template row. */
+
+    /* Make a copy of the template row */
+    newRow = templateRow.cloneNode(true);
+
+    /* Change the form variables e.g. price[x] -> price[rowCount] */
+    elements = newRow.getElementsByTagName("input");
+    for (i = 0; i < elements.length; i++) {
+        element = elements.item(i);
+        s = null;
+        s = element.getAttribute("name");
+        if (s == null)
+            continue;
+        t = s.split("[");
+        if (t.length < 2)
+            continue;
+        s = t[0] + "[" + rowCount.toString() + "]";
+        element.setAttribute("name", s);
+        element.value = "";
+    }
+
+    /* Add the newly-created row to the table */
+    templateRow.parentNode.appendChild(newRow);
+    return true;
+}
+</script>
+
+{{-- Tambah data sales people --}}
+<script>
+    function addRow() {
+        /* Declare variables */
+        var elements, templateRow, rowCount, row, className, newRow, element;
+        var i, s, t;
+
+        /* Get and count all "tr" elements with class="row".    The last one will
+         * be serve as a template. */
+        if (!document.getElementsByTagName)
+            return false; /* DOM not supported */
+        elements = document.getElementsByTagName("tr");
+        templateRow = null;
+        rowCount = 0;
+        for (i = 0; i < elements.length; i++) {
+            row = elements.item(i);
+
+            /* Get the "class" attribute of the row. */
+            className = null;
+            if (row.getAttribute)
+                className = row.getAttribute('class')
+            if (className == null && row.attributes) {    // MSIE 5
+                /* getAttribute('class') always returns null on MSIE 5, and
+                 * row.attributes doesn't work on Firefox 1.0.    Go figure. */
+                className = row.attributes['class'];
+                if (className && typeof(className) == 'object' && className.value) {
+                    // MSIE 6
+                    className = className.value;
+                }
+            }
+
+            /* This is not one of the rows we're looking for.    Move along. */
+            if (className != "row_to_clone")
+                continue;
+
+            /* This *is* a row we're looking for. */
+            templateRow = row;
+            rowCount++;
+        }
+        if (templateRow == null)
+            return false; /* Couldn't find a template row. */
+
+        /* Make a copy of the template row */
+        newRow = templateRow.cloneNode(true);
+
+        /* Change the form variables e.g. price[x] -> price[rowCount] */
+        elements = newRow.getElementsByTagName("input");
+        for (i = 0; i < elements.length; i++) {
+            element = elements.item(i);
+            s = null;
+            s = element.getAttribute("name");
+            if (s == null)
+                continue;
+            t = s.split("[");
+            if (t.length < 2)
+                continue;
+            s = t[0] + "[" + rowCount.toString() + "]";
+            element.setAttribute("name", s);
+            element.value = "";
+        }
+
+        /* Add the newly-created row to the table */
+        templateRow.parentNode.appendChild(newRow);
+        return true;
+    }
+</script>
+
+<script>
+    $(document).ready(function(){
+        $("#dataDana").on('click','.removeDana',function(){
+            $(this).parent().parent().remove();
+        });
+    });
+</script>
+
+{{-- Map Leaflet JS --}}
+<script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
+    integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+    crossorigin=""></script>
+<script>
+    var mapCenter = [{{ request('latitude', config('leaflet.map_center_latitude')) }}, {{ request('longitude', config('leaflet.map_center_longitude')) }}];
+    var map = L.map('mapid').setView(mapCenter, {{ config('leaflet.zoom_level') }});
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var marker = L.marker(mapCenter).addTo(map);
+    function updateMarker(lat, lng) {
+        marker
+        .setLatLng([lat, lng])
+        .bindPopup("Your location :  " + marker.getLatLng().toString())
+        .openPopup();
+        return false;
+    };
+
+    map.on('click', function(e) {
+        let latitude = e.latlng.lat.toString().substring(0, 15);
+        let longitude = e.latlng.lng.toString().substring(0, 15);
+        $('#latitude').val(latitude);
+        $('#longitude').val(longitude);
+        updateMarker(latitude, longitude);
+    });
+
+    var updateMarkerByInputs = function() {
+        return updateMarker( $('#latitude').val() , $('#longitude').val());
+    }
+    $('#latitude').on('input', updateMarkerByInputs);
+    $('#longitude').on('input', updateMarkerByInputs);
+</script>
