64517c9605103373816c65f252b5138ab7ffb8bc
rimbaborne <gagangborne@gmail.com>
2022-07-18 06:54:56 +0800

    pembaruan inbox

--- database/migrations/2022_06_10_031553_create_proposals_table.php
+++ database/migrations/2022_06_10_031553_create_proposals_table.php
@@ -18,6 +18,9 @@ class CreateProposalsTable extends Migration
             $table->uuid('uuid');
             $table->string('no_proposal')->nullable(); // NOMOR PROPOSAL nomor proposal / tahun / bulan / tanggal / jenis activity / dealer
             $table->integer('user_proposal')->nullable(); // id pembuat
+            $table->integer('user_approval')->nullable(); // id pembuat
+            $table->boolean('inbox_d')->nullable(); // id pembuat
+            $table->boolean('inbox_md')->nullable(); // id pembuat
             $table->string('status_proposal')->nullable(); // 1 draft / 2 Waiting Approval / 3 Partial Approval / 4  Final / 5 Revise (kembali approval terakhir)  / 6 Rejected
             $table->string('status_event_proposal')->nullable(); // 1 Proses Pengajuan / 2 Akan Berjalan / 3 Sedang Berjalan / 4 Selesai
             $table->string('dealer_proposal')->nullable(); // id dealer
